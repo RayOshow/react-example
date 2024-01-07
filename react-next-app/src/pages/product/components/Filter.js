d7d3f9ef178d471c025d4d983ccc29bbe30a446e
RayOshow <agundle6@gmail.com>
2024-01-07 14:19:54 +0900

    next.js 기본 테스트 프로젝트

---
+++ react-next-app/src/pages/product/components/Filter.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./Filter.css"; // CSS 파일 임포트
+
+function Filter({ filter, onFilterChange }) {
+  const [selectedBrand, setSelectedBrand] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState(0);
+
+  const handleBrandChange = (index) => {
+    setSelectedBrand(index);
+    onFilterChange({ brand: index, category: selectedCategory });
+  };
+
+  const handleCategoryChange = (index) => {
+    setSelectedCategory(index);
+    onFilterChange({ brand: selectedBrand, category: index });
+  };
+
+  return (
+    <div className="filter-container">
+      {/* 브랜드 필터 */}
+      <div className="filter-item">
+        <h3>브랜드</h3>
+        {filter.brand.map((brand, index) => (
+          <label key={brand}>
+            <input
+              type="radio"
+              value={brand}
+              checked={selectedBrand === index}
+              onChange={() => handleBrandChange(index)}
+            />
+            {brand}
+          </label>
+        ))}
+      </div>
+
+      {/* 카테고리 필터 */}
+      <div className="filter-item">
+        <h3>카테고리</h3>
+        {filter.category.map((category, index) => (
+          <label key={category}>
+            <input
+              type="radio"
+              value={category}
+              checked={selectedCategory === index}
+              onChange={() => handleCategoryChange(index)}
+            />
+            {category}
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
---
+++ react-next-app/src/pages/product/index.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import ProductList from "./components/ProductList";
+import Filter from "./components/Filter";
+import "./product.css";
+
+import Head from 'next/head';
+
+function product() {
+  console.log("Main이 렌더링 됐다.");
+
+  // productList라는 상태를 생성하고, 초기값으로 빈 배열([])을 할당합니다.
+  const [productList, setProductList] = useState([]);
+  const [filter, setFilter] = useState(null);
+  const [selectedFilter, setSelectedFilter] = useState({
+    brand: 0,
+    category: 0,
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:5001/filter")
+      .then((response) => response.json())
+      .then((data) => setFilter(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  useEffect(() => {
+
+    console.log('12312313')
+    let uri = "http://localhost:5001/items";
+
+    let separator = "?";
+
+    if (selectedFilter.brand > 0) {
+      uri += separator + "brand=Brand" + filter.brand[selectedFilter.brand];
+      separator = "&";
+    }
+
+    if (selectedFilter.category > 0) {
+      uri += separator + "category=" + filter.category[selectedFilter.category];
+    }
+
+    fetch(uri)
+      .then((response) => response.json())
+      .then((data) => setProductList(data))
+      .catch((error) => console.error("Error:", error));
+  }, [selectedFilter]); // searchKeyword 가 변경될 때마다 API가 재호출 됩니다.
+
+  function handleFilterChange(selectedFilter) {
+    setSelectedFilter(selectedFilter);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Product Page</title>
+        <meta name="description" content="이건 상품 리스트 페이지" />
+        <meta name="keywords" content="product, website" />
+      </Head>
+      <div className="mainContainer">
+        {filter && <Filter filter={filter} onFilterChange={handleFilterChange} />}
+
+        {productList && productList.length > 0 ? (
+          <ProductList products={productList} />
+        ) : (
+          <div className="notFound"> 해당 브랜드의 상품이 없습니다.</div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default product;
